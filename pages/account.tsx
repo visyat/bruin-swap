ac863e246c55a4ec23aa8265186b7e80db383f2e
Aditya Pai <paiaditya@outlook.com>
2024-03-04 01:24:01 -0800

    added profile and class buttons on account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,9 +1,37 @@
 import RootLayout from '../app/layout';
+import styles from '../styles/Account.module.css';
+import React, { useState } from 'react';
+
 
 const Account = () => {
+
+  const [content, setContent] = useState('');
+  const populateContent = () => {
+    // Update the content state
+    setContent('This is the dynamically populated content.');
+  };
   return (
-    <RootLayout>
-      <div>Account</div>
+    <RootLayout >
+      <div id="content">
+                {/* Render the dynamic content */}
+                {content && <p>{content}</p>}
+            </div>
+      <div>
+        <button className={styles.profileButton} onClick={populateContent}>Profile Information</button>
+      </div>
+      
+      <div>
+        <button className={styles.currentButton}>Current Classes</button>
+      </div>
+      
+      <div>
+        <button className={styles.wishlistButton}>Wishlist Classes</button>
+      </div>
+
+      <div>
+        <span className={styles.divider}> </span>
+      </div>
+
     </RootLayout>
   )
 }
