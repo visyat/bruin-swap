5c93be7a9962300face5f700ad34bc135725b8d9
rathull <notifyrathul@gmail.com>
2024-03-04 21:00:55 -0800

    merging in frontend repo

---
+++ course-marketplace/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from './globals.css';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main className={ styles.main }>
+      Base
+    </main>
+  );
+}
+
+export default Home;
