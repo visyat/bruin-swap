8cb25b99d39884f2cef6ed0b679c5c74f2f84838
rathull <notifyrathul@gmail.com>
2024-03-04 21:02:50 -0800

    refactor client naming

---
+++ client/constants/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Course Marketplace",
+    description: "Development version of course marketplace",
+};
---
+++ client/pages/account.tsx
@@ -0,0 +1,11 @@
+import RootLayout from '../app/layout';
+
+const Account = () => {
+  return (
+    <RootLayout>
+      <div>Account</div>
+    </RootLayout>
+  )
+}
+
+export default Account;
