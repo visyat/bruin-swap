e3efd906b37dbb8d91f3307598afc9009336b4c1
Aditya Pai <paiaditya@outlook.com>
2024-03-04 18:36:41 -0800

    added input fields to form file

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,37 +1,89 @@
+import { useState } from 'react';
 import RootLayout from '../app/layout';
 import styles from '../styles/Account.module.css';
-import React, { useState } from 'react';
 
 
 const Account = () => {
+  const [output, setOutput] = useState('');
+  const [profileInfo, setProfileInfo] = useState({
+    email: '',
+    fullName: '',
+    major: '',
+    graduationYear: ''
+  });
 
-  const [content, setContent] = useState('');
-  const populateContent = () => {
-    // Update the content state
-    setContent('This is the dynamically populated content.');
+  const handleButtonClick = (text: string) => {
+    setOutput(text);
   };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, fieldName: string) => {
+    setProfileInfo(prevProfileInfo => ({
+      ...prevProfileInfo,
+      [fieldName]: event.target.value
+    }));
+  };
+
+  let content;
+  if (output === 'Profile Information') {
+    content = (
+      <div>
+        <h2>Profile Information</h2>
+        <div>
+          <label>Email: </label>
+          <input type="email" className={styles.inputForm} value={profileInfo.email}  
+          onChange={(e) => handleInputChange(e, 'email')} 
+          placeholder='Enter Email...'/>
+        </div>
+        <div className={styles.spacing}>
+          <label>Full Name: </label>
+          <input type="text" className={styles.inputForm} value={profileInfo.fullName}  
+          onChange={(e) => handleInputChange(e, 'fullName')} 
+          placeholder = 'Full Name...'/>
+         
+        </div>
+        <div className={styles.spacing}>
+          <label>Major: </label>
+          <input type="text" className={styles.inputForm} value={profileInfo.major} 
+          onChange={(e) => handleInputChange(e, 'major')} 
+          placeholder = 'Enter Major...' />
+        </div>
+        <div className={styles.spacing}> 
+          <label>Graduation Year: </label>
+          <input type="number" className={styles.inputForm} 
+          value={profileInfo.graduationYear} onChange={(e) => handleInputChange(e, 'graduationYear')}
+          placeholder = 'Enter Grad Year...' />
+        </div>
+      </div>
+    );
+  } else {
+    content = <div>No content selected</div>;
+  }
+
+
   return (
     <RootLayout >
-      <div id="content">
-                {/* Render the dynamic content */}
-                {content && <p>{content}</p>}
+      <div>
+            <div>
+                <button className={styles.profileButton} onClick={() => handleButtonClick('Profile Information')}>Profile Information</button>
+                
             </div>
-      <div>
-        <button className={styles.profileButton} onClick={populateContent}>Profile Information</button>
-      </div>
-      
-      <div>
-        <button className={styles.currentButton}>Current Classes</button>
-      </div>
-      
-      <div>
-        <button className={styles.wishlistButton}>Wishlist Classes</button>
-      </div>
+            
+            <div>
+                <button className={styles.currentButton} onClick={() => handleButtonClick('Current Classes')}>Current Classes</button>
+            </div>
+            
+            <div>
+                <button className={styles.wishlistButton} onClick={() => handleButtonClick('Wishlist Classes')}>Wishlist Classes</button>
+            </div>
 
-      <div>
-        <span className={styles.divider}> </span>
-      </div>
+            <div>
+                <span className={styles.divider}></span>
+            </div>
 
+            <div>
+                <span className={styles.outputContainer}>{content}</span>
+            </div>
+        </div>
     </RootLayout>
   )
 }
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,9 +1,34 @@
 import RootLayout from '../app/layout';
+import React, {useState } from 'react';
+import styles from '../styles/Search.module.css'
+
+
 
 const Search = () => {
+
+  const [search, setSearch] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setSearch(event.target.value); 
+  }
+ 
+
+    
   return (
     <RootLayout>
       <div>Search</div>
+      <div className={styles.searchInput}>
+      <input 
+            type="text"
+            id="header-search"
+            placeholder="Search blog posts"
+            name="s" 
+            defaultValue= {search}
+            onChange={handleChange}
+            
+        />
+        </div>
     </RootLayout>
   )
 }
