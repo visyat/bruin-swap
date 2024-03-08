d08ded033e1f1631b510a26b5015044efdbcc7cb
rathull <notifyrathul@gmail.com>
2024-03-08 13:40:49 -0800

    completed search page

--- client/components/classcard.tsx
+++ client/components/classcard.tsx
@@ -10,43 +10,108 @@ import {
     CardHeader,
     CardPreview,
 } from "@fluentui/react-components";
-  
+import { ArrowSwapFilled } from '@fluentui/react-icons';
+
 const useStyles = makeStyles({
-    card: {
+    card: { // fill div
         ...shorthands.margin('auto'),
-        width: '720px',
+        width: '100%',
         maxWidth: '100%',
+        minHeight: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between', // ensure even footer
+    }, details: { // department, number, title, instructor
+        display: 'flex',
+        flexDirection: 'column',
+    }, header: { 
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexGrow: 0,
+    }, preview: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        // padding: '10px',
+        flexGrow: 0,
+    }, footer: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        // padding: '10px',
+        flexGrow: 0,
+    }, swap: {
+        backgroundColor: 'transparent',
+        ...shorthands.border('2px', 'solid', 'rgb(19, 19, 19)'),
+        minWidth: '100px',
+        paddingLeft: '10px',
+        paddingRight: '10px',
+        paddingTop: '5px',
+        paddingBottom: '5px',
+        borderRadius: '15px',    
     }
 })
 
-interface ClassProps {
-    data: {
-        id: number;
-        classDept: string;
-        classNum: string;
-        classTitle: string;
-        instructor: string;
-        lecture: string;
-    };
+interface ClassCard {
+    id: number;
+    classDept: string;
+    classNum: string;
+    classTitle: string
+    instructor: string;
+    lecture: string;
+};
+
+interface CardProps {
+    data: ClassCard;
 }
 
-const Class: React.FC<ClassProps> = ({ data }) => {
+const cardTokens = {
+    childrenGap: '12px',
+    padding: '12px',
+};  
+
+const ClassCard: React.FC<CardProps> = ({ data }) => {
     const { id, classDept, classNum, classTitle, instructor, lecture } = data;
     const styles = useStyles();
 
 	return (
-        <Card className={styles.card}>
-            <CardHeader 
-            
-            />
-            <CardPreview 
-            
-            />
-            <CardFooter
-
-            />
+        <Card 
+            className={styles.card}
+            appearance="filled"
+            as="div"
+            size="small"
+        >
+        <CardHeader 
+            className={styles.header}
+            header={
+                <div className={styles.details}>
+                    <Body1>{`${classDept} ${classNum}: ${classTitle}`}</Body1>
+                </div>
+            }
+            description={
+                <Caption1>{`${instructor}`}</Caption1>
+            }
+        />
+        {/* Include CardPreview for wanted classes */}
+        <CardFooter
+            className={styles.footer}
+            action={
+                <Button 
+                    className={styles.swap} 
+                    icon={<ArrowSwapFilled />}
+                    as="button"
+                    appearance="primary"
+                    shape="rounded"
+                >
+                    Swap!
+                </Button>
+            }
+        >
+            <Caption1>Lecture: {lecture}</Caption1>
+        </CardFooter>
         </Card>
 	);
 };
 
-export default Class;
+export default ClassCard;
--- client/pages/search.tsx
+++ client/pages/search.tsx
@@ -1,23 +1,59 @@
+import { makeStyles, shorthands } from "@fluentui/react-components";
 import { OpenCardMode } from '@fluentui/react';
-import Class from '../components/classcard';
+import ClassCard from '../components/classcard';
 import { CLASSES } from '../constants/temp_data';
 
 interface Class {
     id: number;
     classDept: string;
     classNum: string;
-    prof: string;
+    classTitle: string
+    instructor: string;
     lecture: string;
 };
 
+const useStyles = makeStyles({
+	searchTitle: {
+		marginTop: '10px',
+		marginBottom: '10px',
+		textAlign: 'center',
+		fontSize: '30px',
+	}, classes: {
+		width: '100%',
+		height: '100%',
+		marginLeft: '0 auto',
+		marginRight: '0 auto',
+		display: 'grid',
+		gridTemplateColumns: '2fr 2fr 2fr',
+		placeItems: 'center',
+		alignItems: 'center',
+		gridGap: '100px',
+	}, card: {
+		cursor: 'pointer',
+		width: '100%',
+		minHeight: '100px',
+		height: '100%',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	}
+});
+
 const Search = () => {
+	const styles = useStyles();
+
 	return (
-		<div className="shop">
-			<div className="shotTitle">
-				<h1>BruinSwap Listings</h1>
+		<div>
+			<div className={styles.searchTitle}>
+				<h1>Results</h1>
 			</div>
-			<div className="classes">
-				{CLASSES.map((listing: Class) => <Class data={listing}/>)}
+			<div className={styles.classes}>
+				{CLASSES.map((listing: Class) => (
+					<div className={styles.card} key={listing.id}>
+						<ClassCard data={listing}/>
+					</div>
+				))}
 			</div>
 		</div>
 	)
