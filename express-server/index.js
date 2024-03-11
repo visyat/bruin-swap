48695b512520922498b3669d60dc8a525130bd57
Vishal Yathish <vishal.yathish@gmail.com>
2024-03-11 00:53:39 -0700

    Switched to JWT Tokens for API calls (authorization).

--- express-server/index.js
+++ express-server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import { getAllUsers, checkValidUser, getUserInfoByID, authentication, getAllTransactions, getTransactionsByUser, getTransactionsByID, getTransactionsByDept, getTransactionsByCourseNum, getAllClasses, getClassesBySC, getWishlistByUser, getEnrollmentsByUser, addNewUser, addNewClass, addNewTransaction, addNewWishlistEntry, addNewEnrollmentEntry, updateUserInfoByID, updateCourseInfoByID, updateTransactionInfoByID, deleteUser, deleteCourse, deleteSection, deleteTransaction, deleteWishlistEntry, deleteEnrollmentEntry } from './queries.js';
+import { getAllUsers, checkValidUser, checkUserAlreadyExists, getUserInfoByID, getUserInfoByJWT, authentication, getAllTransactions, getTransactionsByUser, getTransactionsByID, getTransactionsByDept, getTransactionsByCourseNum, getAllClasses, getClassesBySC, getWishlistByUser, getEnrollmentsByUser, addNewUser, addNewClass, addNewTransaction, addNewWishlistEntry, addNewEnrollmentEntry, updateUserInfoByJWT, updateCourseInfoByID, updateTransactionInfoByID, deleteUser, deleteCourse, deleteSection, deleteTransaction, deleteWishlistEntry, deleteEnrollmentEntry } from './queries.js';
 
 const app = express()
 const port = 3000
@@ -13,18 +13,20 @@ app.get('/', (request, response) => {
 })
 
 app.get('/users', getAllUsers)
-app.get('/check-user/:user_id', checkValidUser)
-app.get('/users/:user_id', getUserInfoByID)
+app.get('/check-valid-user/:user_id', checkValidUser)
+app.get('/check-user-exists/:user_id', checkUserAlreadyExists)
+app.get('/users-id/:user_id', getUserInfoByID)
+app.get('/users/:user_jwt', getUserInfoByJWT)
 app.get('/login/:user_id/:passwd', authentication)
 app.get('/transactions', getAllTransactions)
-app.get('/transactions-by-user/:user_id', getTransactionsByUser)
+app.get('/transactions-by-user/:user_jwt', getTransactionsByUser)
 app.get('/transactions-by-tid/:transaction_id', getTransactionsByID)
 app.get('/transactions-by-dept/:dept', getTransactionsByDept)
 app.get('/transactions-by-cn/:course_num', getTransactionsByCourseNum)
 app.get('/classes', getAllClasses)
 app.get('/classes/:section_code', getClassesBySC)
-app.get('/wishlist/:user_id', getWishlistByUser)
-app.get('/enrollments/:user_id', getEnrollmentsByUser)
+app.get('/wishlist/:user_jwt', getWishlistByUser)
+app.get('/enrollments/:user_jwt', getEnrollmentsByUser)
 
 app.post('/users', addNewUser)
 app.post('/classes', addNewClass)
@@ -32,16 +34,16 @@ app.post('/transactions', addNewTransaction)
 app.post('/wishlist', addNewWishlistEntry)
 app.post('/enrollments', addNewEnrollmentEntry)
 
-app.put('/users/:user_id', updateUserInfoByID)
+app.put('/users/:user_jwt', updateUserInfoByJWT)
 app.put('/courses/:section_code', updateCourseInfoByID)
 app.put('/transactions/:transaction_id', updateTransactionInfoByID)
 
-app.delete('/users/:user_id', deleteUser)
+app.delete('/users/:user_jwt', deleteUser)
 app.delete('/courses/:section_code', deleteSection)
 app.delete('/courses/:dept/:course_num', deleteCourse)
 app.delete('/transactions/:transaction_id', deleteTransaction)
-app.delete('/wishlist/:user_id/:section_id', deleteWishlistEntry)
-app.delete('/enrollments/:user_id/:section_id', deleteEnrollmentEntry)
+app.delete('/wishlist/:user_jwt/:section_id', deleteWishlistEntry)
+app.delete('/enrollments/:user_jwt/:section_id', deleteEnrollmentEntry)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
